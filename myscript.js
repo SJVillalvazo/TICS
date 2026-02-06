59481a07bbd4d99485adb75d4ebf1ad2d8a32707
SJVillalvazo <svillalvazo3@ucol.mx>
2026-02-06 08:31:08 -0600

    mayor de edad

---
+++ myscript.js
@@ -0,0 +1,9 @@
+let edad = parseInt(prompt('ingresa tu edad'));
+if (edad >= 18) console.log('MAYOR DE EDAD');
+    else console.log('MENOR DE EDAD');
+
+/*
+if (edad >= 18) {
+    alert("Eres mayor de edad")
+}
+*/
