28e75f35c53de9c25625264561bf1104f546a097
SJVillalvazo <svillalvazo3@ucol.mx>
2026-02-20 20:59:34 -0600

    agenda

--- agenda/JS/agenda.js
+++ agenda/JS/agenda.js
@@ -1,39 +1,64 @@
-document.write("Una agenda");
-
-let opcion = parseInt(prompt("Escoge una opción del \n 1-Agregar \n 2-Listar \n 3-Salir"));
-
 let seguir = true;
-
 let datos = [];
 
-while(seguir) {
-    let opcion = parseInt(prompt("Escoge una opción del \n 1-Agregar un contacto \n 2-Listar contactos \n 3-Salir"));
+while(seguir){
+    let opcion = parseInt(prompt(
+        "Bienvenido a tu agenda personal \n" +
+        "1. Agregar contacto \n" +
+        "2. Lista de contactos \n" +
+        "3. Eliminar contacto \n" +
+        "4. Salir"
+    ));
+
     switch(opcion) {
         case 1:
-            alert("Escogiste la opción 1");
             let nombre = prompt("Ingresa tu nombre");
             let edad = parseInt(prompt("Ingresa tu edad"));
             let telefono = prompt("Ingresa tu telefono");
             let id_contacto = datos.length + 1;
-            let agenda = {id_contacto, nombre, edad, telefono};
-            agenda.id_contacto = id_contacto;
+            let agenda = {id_contacto, nombre, edad, telefono };
+            
             agenda.nombre = nombre;
             agenda.edad = edad;
             agenda.telefono = telefono;
-
-            // ingresar el nombre al array
-            datos.push(nombre);
-            console.table(datos);
+            agenda.id_contacto = id_contacto;
+            datos.push(agenda);
+            console.log(datos);
+            alert("Contacto agregado correctamente");
             break;
         case 2:
-            alert("Escogiste la opción 2");
+            let todos = "";
+            for(let i = 0; i < datos.length; i++) {
+                todos += "Nombre: " + datos[i].nombre + "\n";
+                todos += "Edad: " + datos[i].edad + "\n";
+                todos += "Telefono: " + datos[i].telefono + "\n";
+                todos += "-----------------------\n";
+            }
+
+            if(datos.length === 0) {
+                alert("No hay contactos registrados");
+            } else {
+                alert(todos);
+                console.table(datos);
+            }
             break;
         case 3:
-            alert("Escogiste la opción 3, salir");
-            seguir = false;
+            let id_eliminar = parseInt(prompt("Ingresa el ID del contacto a eliminar"));
+            let index_contacto = datos.findIndex(contacto => contacto.id_contacto === id_eliminar);
+
+            if(index_contacto !== -1) {
+                datos.splice(index_contacto, 1);
+                alert("Contacto eliminado exitosamente");
+            } else {
+                alert("ID de contacto no existente");
+            }    
+        case 4:
+            if(confirm("¿Estas seguro de que quieres salir?")) {
+                seguir = false;
+            }
             break;
         default:
             alert("No recibí una opción válida");
-            break;
     }
+    alert("¡Gracias por usar tu agenda personal!");
 }
